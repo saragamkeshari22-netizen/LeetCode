befc350b346dabad944a68e0b97f8e3a5bcefb08
saragamkeshari22-netizen <saragamkeshari22@gmail.com>
2026-09-11 21:27:08 +0530

    Time: 2 ms (77.33%), Space: 7.7 MB (65.29%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int fib(int n) {
+        int prev = 0;
+        int curr = 1;
+
+        for(int i = 0; i < n; i++) {
+            int next = prev + curr;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+};
