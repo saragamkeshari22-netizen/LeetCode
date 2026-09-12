5d0b6b8882e64db216833d24e59ee8d37b52f1ee
saragamkeshari22-netizen <saragamkeshari22@gmail.com>
2026-09-12 23:24:39 +0530

    Time: 3 ms (9.22%), Space: 10 MB (8.58%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,30 +2,13 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-
-        for(char ch : s) {
-            if(ch == '(' || ch == '{' || ch == '[') {
-                st.push(ch);
-            }
-            else {
-                if(st.empty()) {
-                    return false;
-                }
-
-                if(ch == ')' && st.top() != '(') {
-                    return false;
-                }
-                if(ch == '}' && st.top() != '{') {
-                    return false;
-                }
-                if(ch == ']' && st.top() != '[') {
-                    return false;
-                }
-
-                st.pop();
-            }
+        for(char c : s) {
+            if(c=='(') st.push(')');
+            else if(c=='{') st.push('}');
+            else if(c=='[') st.push(']');
+            else if(st.empty() || st.top()!=c) return false;
+            else st.pop();
         }
-
         return st.empty();
     }
 };
