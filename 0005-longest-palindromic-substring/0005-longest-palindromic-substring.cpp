ed3efe5da93ffa3761168162e4233e5e0bd1ecb8
saragamkeshari22-netizen <saragamkeshari22@gmail.com>
2026-09-21 00:07:15 +0530

    Time: 18 ms (42.07%), Space: 22 MB (31.69%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        string ans = "";
+
+        for (int i = 0; i < s.length(); i++) {
+
+            // Odd length
+            int l = i, r = i;
+
+            while (l >= 0 && r < s.length() && s[l] == s[r]) {
+                if (r - l + 1 > ans.length())
+                    ans = s.substr(l, r - l + 1);
+
+                l--;
+                r++;
+            }
+
+            // Even length
+            l = i;
+            r = i + 1;
+
+            while (l >= 0 && r < s.length() && s[l] == s[r]) {
+                if (r - l + 1 > ans.length())
+                    ans = s.substr(l, r - l + 1);
+
+                l--;
+                r++;
+            }
+        }
+
+        return ans;
+    }
+};
